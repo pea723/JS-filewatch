3f60c1320314cbe2bfc5ae19ace36009b5b9b4d6
Natsumi Matsumoto <natsumi@matsumo.to>
2019-10-02 18:19:23 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { file } from '@babel/types';
 
 function App() {
+  const [files, setState] = useState([]);
+  const [contentText, setContent] = useState('');
+
+  const initWatcher = async (e) => {
+    e.preventDefault();
+    const opts = { type: 'openDirectory' };
+    const handle = await window.chooseFileSystemEntries(opts);
+    
+
+    const st = setInterval(async () => {
+      const fileList = [];
+      const entries = await handle.getEntries();
+    for await (const entry of entries) {
+      fileList.push(entry);
+    }
+    setState(fileList);
+    }, 1000);
+  }
+
+  const getContent = (f) => async () => {
+    if (f.isFile) {
+      const content = await f.getFile();
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setContent(reader.result);
+      }
+      reader.readAsText(content);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={
+        {
+          'border': '1px solid #000',
+          'margin': '0 auto',
+          'marginTop': '40px',
+          'marginBottom': '20px',
+          'padding': '20px 30px',
+          'width': '100px',
+        }
+      } onClick={initWatcher}>Click Me to Select Directory</div>
+      <div style={
+        {
+          'marginBottom': '30px',
+        }
+      }>{contentText}</div>
+      <div>
+        {files.map(f => (<div onClick={getContent(f)}>{f.name}</div>))}
+      </div>
     </div>
   );
 }
